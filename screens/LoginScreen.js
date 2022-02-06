ad4d45284f71be8041e82bf7964cd52462c5ab64
Angelo Keirsebilck <angelo.keirsebilck1@telenet.be>
2022-02-06 09:16:17 +0100

    Style login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,15 +1,41 @@
-import { View, Text, Button } from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  ImageBackground,
+  TouchableOpacity,
+} from 'react-native';
 
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import uesAuth from '../hooks/useAuth';
+import { useNavigation } from '@react-navigation/core';
+
+import tw from 'tailwind-rn';
 
 const LoginScreen = () => {
   const { signInWithGoogle, loading } = uesAuth();
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
 
   return (
-    <View>
-      <Text>{loading ? 'Loading...' : 'Login to the app'}</Text>
-      <Button title='Login' onPress={signInWithGoogle} />
+    <View style={tw('flex-1 ')}>
+      <ImageBackground
+        resizeMode='cover'
+        style={tw('flex-1  flex-col justify-end items-center')}
+        source={{ uri: 'https://tinder.com/static/tinder.png' }}
+      >
+        <TouchableOpacity
+          style={[tw('absolute bottom-24 w-52 bg-white p-4 rounded-2xl'), {}]}
+          onPress={signInWithGoogle}
+        >
+          <Text style={tw('text-center font-bold')}>Sign in & Get swiping</Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   );
 };
